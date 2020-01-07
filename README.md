64b80efe923cfb77bfe754dc9cf2dce184af2249
Mike Manticore <king@yahoo.com>
2020-01-07 10:44:20 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RyanMichaelNasalinas.github.io
+
+<h1>Hi thank for visiting my profile</h1>
+
+<h2>You are welcome to view my source for this portfolio site.</h2>
+
+<p>Please take note: that this website is working on Google Chrome Browser and Mozila Firefox, however this website will not work on Internet Explorer because of  the animate on scroll library(will fix this bug in the future), so please open/view this website in Google/Mozila or Microsoft Edge</p>
+
+<p><b>Thank you again,</b></p>
+<p>Ryan Michael Nasalinas</p>
+
---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+//Initiaze animate on scroll
+AOS.init();
+
+AOS.refreshHard();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+// Navbar on scroll
+
+var nav = document.getElementById('navbar');
+
+window.onscroll = function() {
+    //page Y offset or Scroll Y
+    if(window.pageYOffset > 0) {
+        nav.classList.add('scrolled'); 
+    } else {
+        nav.classList.remove('scrolled');
+    }
+}
+
+
+//Select the navigation class
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle Nav
+    burger.addEventListener('click',() => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+        
+        //Animate Links
+        navLinks.forEach((link,index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`;
+            }
+        });
+        //Burger menu animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+
+
+var i = 0;
+var text = "Dreamer + Passionate + Hard Working + Continous Learner";
+
+function typing() {
+
+    if(i < text.length) {
+        document.getElementById('banner-submain').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,100);
+    }
+}
+typing();
+
+//Footer Year
+const date = new Date();
+const year = date.getFullYear();
+
+document.getElementById('year').innerHTML = year;
+
+navSlide();
+
+
